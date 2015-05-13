878dc2f8f046125b0adb1ed1295d497f8515141a
Daniel Fuentes <danielfuentes89@gmail.com>
2015-05-13 00:03:28 -0300

    autenticación en login y session
    se crea el formulario logion en base a codigo kumbia y clases botstrap,
    ademas se agregan variables de session

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -10,6 +10,24 @@ class IndexController extends AppController
     public function index()
     {
         View::template('cover');
+        Load::lib('auth');
+        Load::model('usuarios');
+        if (Input::hasPost("inputEmail3","inputPassword3")){
+            $pwd = Input::post("inputPassword3");
+            $usuario=Input::post("inputEmail3"); 
+            $auth = new Auth("model", "class: usuarios", "login: $usuario", "password: $pwd");
+            if ($auth->authenticate()) {
+                $login = new Usuarios();
+                $us = $login->find_first("login='$usuario'");
+                $edad = $us->edad;
+                Session::set('edad', $edad);
+                Flash::success("Correcto");
+                Router::redirect("principal/index");
+            } else {
+                Flash::error("Usuario o contraseña invalidos");
+                Redirect::to('index');            
+            }
+        }
     }
 
 }
